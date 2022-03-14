a22a55f946bae3af58fac69931b734fdee6903d5
Gabriel Hanssmann <74669444+GabrielHanssmann@users.noreply.github.com>
2022-03-14 17:53:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// MENU
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.menu');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+}
+
+
+
+
+
+
+
